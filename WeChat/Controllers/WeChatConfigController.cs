8b708837ffbe3bbde3600f7907f1c3b6a8555e7f
wangqilong <664660754@qq.com>
2018-12-23 15:13:18 +0800

    update

---
+++ WeChat/Controllers/WeChatConfigController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WeChat.Controllers
+{
+    public class WeChatConfigController : Controller
+    {
+        // GET: WeChatConfig
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        #region 进入管理后台页面页面
+        public ActionResult loginFrom(string name,string pwd)
+        {
+            if (name == "王旗龙" && pwd == "wangqilong")
+            {
+                return View();
+            }
+            else {
+                return null;
+            }
+        }
+        #endregion
+    }
+}
